b03b59d99124dc3d8ba59abd206d2a4463e45b05
Martin Allien <martin@allienworks.net>
2015-11-18 22:28:18 +0100

    Features page

---
+++ features.php
@@ -0,0 +1,79 @@
+<?php include "_head.php" ?>
+
+	<title>Features &bull; Shadow Project</title>
+	<meta property="og:title" content="Features &bull; Shadow Project" />
+	<meta property="og:description" content="???" />
+	<meta property="og:url" content="http://shadowproject.io/features" />
+</head>
+<body>
+
+
+<?php include "_header.php" ?>
+
+
+<div class="row">
+	<div class="medium-4 large-3 columns">
+
+
+		<div id="sidebar">
+			<ul class="nav">
+				<li><a href="/">About</a></li>
+				<li><a href="getting-started">Getting started</a></li>
+				<li><a class="active" href="features">Features</a></li>
+				<li><a href="downloads">Downloads</a></li>
+				<li><a href="documentation">Documentation</a></li>
+				<li><a href="development">Development</a></li>
+				<li><a href="community">Community</a></li>
+				<li><a href="faq">FAQ</a></li>
+				<li><a href="contact">Contact</a></li>
+			</ul>
+		</div><!-- #sidebar -->
+
+
+	</div>
+	<div class="medium-8 large-9 columns">
+
+
+		<div id="content">
+
+      <h2>ShadowChat</h2>
+			<p>
+				ShadowChat is a <abbr title="peer-to-peer">P2P</abbr> Instant Messaging system utilizing state-of-the-art encryption technology to keep your communications private. All messages are encrypted by the proven <strong>AES-256-CBC algorithm</strong>, and distributed between nodes in such a way as to prevent the recipients of messages from being inferred by assailants utilizing sophisticated traffic analysis, even if the assailants can view the entire network and/or run nodes of the network.
+			</p>
+			<p>
+				To eliminate the risk and hassle of sharing passwords, we utilize the proven and trusted method of <strong>Elliptic Curve Diffie-Hellman (ECDH) key exchange</strong>. The Elliptic Curve Digital Signature Algorithm (ECDSA) is used to give you confidence that the messages you received came from the original recipient and remained untouched in propagation. Messages are distributed over the preexisting Shadow P2P network, and a copy of each encrypted message is stored on each node for a period of 48 hours.
+			</p>
+			<p>
+				More info on ShadowChat at <a href="http://doc.shadowproject.io/#encrypted-messaging">doc.shadowproject.io/#encrypted-messaging</a>
+			</p>
+
+			<h2>ShadowMarket</h2>
+
+			<h3>Aplha private testing</h3>
+			<p>
+				More info coming soon.
+			</p>
+
+			<h3>Beta public testing</h3>
+			<p>
+				More info coming soon.
+			</p>
+
+			<h3>Demo</h3>
+			<p>
+				Video coming soon.
+			</p>
+
+			<h3>Roadmap</h3>
+			<p>
+				???
+			</p>
+
+		</div><!-- #content -->
+
+
+	</div>
+</div>
+
+
+<?php include "_footer.php" ?>
