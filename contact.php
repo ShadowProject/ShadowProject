8468799890162515b77955e76251e3e44ee212af
Martin Allien <martin@allienworks.net>
2015-11-19 23:22:46 +0100

    Contact page (w/o form func.)

---
+++ contact.php
@@ -0,0 +1,57 @@
+<?php include "_head.php" ?>
+
+	<title>Contact &bull; Shadow Project</title>
+	<meta property="og:title" content="Contact &bull; Shadow Project" />
+	<meta property="og:description" content="???" />
+	<meta property="og:url" content="http://shadowproject.io/contact" />
+</head>
+<body>
+
+
+<?php include "_header.php" ?>
+
+
+<div class="row">
+	<div class="medium-4 large-3 columns">
+
+
+		<div id="sidebar">
+			<ul class="nav">
+				<li><a href="/">About</a></li>
+				<li><a href="getting-started">Getting started</a></li>
+				<li><a href="features">Features</a></li>
+				<li><a href="documentation">Documentation</a></li>
+				<li><a href="development">Development</a></li>
+				<li><a href="bug-bounties">Bug Bounties</a></li>
+				<li><a href="community">Community</a></li>
+				<li><a href="faq">FAQ</a></li>
+				<li><a class="active" href="contact">Contact</a></li>
+			</ul>
+		</div><!-- #sidebar -->
+
+
+	</div>
+	<div class="medium-8 large-9 columns">
+
+
+		<div id="content">
+			<h2>Contact us</h2>
+			<p>
+				Get in touch with the team:
+			</p>
+
+			<form class="contact">
+				<textarea name="message" rows="8" placeholder="Your message"></textarea>
+				<input type="text" name="name" value="" placeholder="Name">
+				<input type="email" name="email" value="" placeholder="Email">
+				<input type="submit" class="button" name="send" value="Send message">
+			</form>
+
+		</div><!-- #content -->
+
+
+	</div>
+</div>
+
+
+<?php include "_footer.php" ?>
